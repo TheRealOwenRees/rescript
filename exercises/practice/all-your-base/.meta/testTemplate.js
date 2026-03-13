102267b580862a2de6c7d92b0390d839d8614669
Owen <owenrees.dev@gmail.com>
2026-03-13 11:53:02 +0100

    update test to output array option

--- exercises/practice/all-your-base/.meta/testTemplate.js
+++ exercises/practice/all-your-base/.meta/testTemplate.js
@@ -1,17 +1,25 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { stringEqual } from "../../../../test_generator/assertions.js";
+import { assertEqual } from "../../../../test_generator/assertions.js";
 import { generateTests } from '../../../../test_generator/testGenerator.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const slug = path.basename(path.resolve(__dirname, '..'))
 
 // EDIT THIS WITH YOUR ASSERTIONS
-export const assertionFunctions = [ stringEqual ]
+export const assertionFunctions = [ assertEqual ]
 
 // EDIT THIS WITH YOUR TEST TEMPLATES
 export const template = (c) => {
-  return `stringEqual(~message="${c.description}", hello(), "${c.expected}")`
+  const { inputBase, digits, outputBase } = c.input
+
+  const digitsArrStr = `[${digits.join(', ')}]`
+
+  const expectedStr = (Array.isArray(c.expected))
+    ? `Some([${c.expected.join(', ')}])`
+    : "None"
+
+  return `assertEqual(~message="${c.description}", rebase(${inputBase}, ${digitsArrStr}, ${outputBase}), ${expectedStr})`
 }
 
 generateTests(__dirname, slug, assertionFunctions, template)
