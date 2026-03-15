e525ff6a1157979c5239b938f12ce18f18cceb10
Owen Rees <owenrees.dev@gmail.com>
2026-03-15 10:58:23 +0100

    generate test cases

--- exercises/practice/raindrops/.meta/testTemplate.js
+++ exercises/practice/raindrops/.meta/testTemplate.js
@@ -1,17 +1,17 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { stringEqual } from "../../../../test_generator/assertions.js";
+import { assertEqual } from "../../../../test_generator/assertions.js";
 import { generateTests } from '../../../../test_generator/testGenerator.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const slug = path.basename(path.resolve(__dirname, '..'))
 
 // EDIT THIS WITH YOUR ASSERTIONS
-export const assertionFunctions = [ stringEqual ]
+export const assertionFunctions = [ assertEqual ]
 
 // EDIT THIS WITH YOUR TEST TEMPLATES
 export const template = (c) => {
-  return `stringEqual(~message="${c.description}", hello(), "${c.expected}")`
+  return `assertEqual(~message="${c.description}", convert(${c.input.number}), "${c.expected}")`
 }
 
 generateTests(__dirname, slug, assertionFunctions, template)
